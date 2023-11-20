8744b13746a72c06d9a24f2a1d76accfaa82535a
Xinboshin Avergini <106208768+XinboshinAvergini@users.noreply.github.com>
2023-11-20 23:10:06 +0300

    Реконструктуризация компонентов сайта

--- resources/js/store/modules/header/index.js
+++ resources/js/store/modules/header/index.js
@@ -11,35 +11,42 @@ const state = {
                         {
                             title: 'Сервера 1.7.10',
                             child: [
-                                {title: 'DivineRPG', to: {name: 'page', params: {name: 'divinerpg'}}},
-                                {title: 'DTM', to: {name: 'page', params: {name: 'dtm'}}},
-                                {title: 'Galactic', to: {name: 'page', params: {name: 'galactic'}}},
-                                {title: 'TechnoMagicSky', to: {name: 'page', params: {name: 'technomagicsky'}}},
-                            ]
-                        }
-                    ],
-                    [
-                        {
-                            title: 'Миниигры',
-                            child: [
-                                {title: 'MiniGames', to: {name: 'page', params: {name: 'minigames'}}},
+                                {title: 'Sandbox', to: {name: 'page', params: {name: 'sandbox'}}},
+
+                                {title: 'Technology', to: {name: 'page', params: {name: 'technology'}}},
+                                {title: 'MagicRPG', to: {name: 'page', params: {name: 'magic_rpg'}}},
+                                {title: 'DivineRPG', to: {name: 'page', params: {name: 'rpg'}}}
                             ]
                         },
                         {
                             title: 'Сервера 1.12.2',
                             child: [
-                                {title: 'Technology', to: {name: 'page', params: {name: 'nanotech'}}},
-                                {title: 'Mysterius', to: {name: 'page', params: {name: 'magic-'}}},
-                                {title: 'NanoTech', to: {name: 'page', params: {name: 'nanotech-new'}}},
+                                {title: 'Mysterius', to: {name: 'page', params: {name: 'mysterius'}}},
                                 {title: 'Pixelmon', to: {name: 'page', params: {name: 'pixelmon'}}},
+                                {title: 'Infected', to: {name: 'page', params: {name: 'infected'}}},
                             ]
                         },
+                    ],
+                    [
                         {
                             title: 'Сервера 1.16.5',
                             child: [
                                 {title: 'Architechnica', to: {name: 'page', params: {name: 'architechnica'}}},
+                                {title: 'Vanilla+', to: {name: 'page', params: {name: 'vanilla_plus'}}},
                             ]
-                        }
+                        },
+                        {
+                            title: 'Сервера 1.19.3',
+                            child: [
+                                {title: 'Vanilla', to: {name: 'page', params: {name: 'vanilla'}}},
+                            ]
+                        },
+                        {
+                            title: 'Мини-игры',
+                            child: [
+                                {title: 'MiniGames', to: {name: 'page', params: {name: 'minigames'}}},
+                            ]
+                        },
                     ],
                 ]
             },
@@ -52,7 +59,8 @@ const state = {
                     {title: 'Команды', to: {name: 'page', params: {name: 'commands'}}},
                     {title: 'Банлист', to: {name: 'banlist'}},
                     {title: 'Хочу в команду!', to: {name: 'moderentry'}},
-                    {title: 'Команда проекта', to: {name: 'page', params: {name: 'team'}}}
+                    {title: 'Команда проекта', to: {name: 'page', params: {name: 'team'}}},
+                    {title: 'Приватные сервера', to: {name: 'waitlistentry'}}
                 ]
             },
             {
